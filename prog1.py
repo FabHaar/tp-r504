292744b6fc6dd50225cf4bd4aab58f5b8943b414
FabHaar <fabienhaar@gmail.com>
2023-09-07 09:48:40 +0200

    Q2

--- prog1.py
+++ prog1.py
@@ -1,1 +1,13 @@
+import fonctions as f
+
 print(" Hello, World ! ")
+
+while True :
+	nombre = int(input ("Saisir un nombre pour afficher son carré : "))
+	print (nombre*nombre)
+	
+	v1 = int(input("indiquer un nombre : "))
+	v2 = int(input("et sa puissance : "))
+	res = f.puissance (v1,v2)
+	print (res)
+
