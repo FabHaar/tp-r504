7accd48dee62ff5659ce9ffe079c6188716fcb79
FabHaar <fabienhaar@gmail.com>
2023-09-08 14:55:19 +0200

    correction de fonctions

--- fonctions.py
+++ fonctions.py
@@ -3,10 +3,5 @@ def puissance(a,b):
 		raise TypeError ("Only integers are allowed")
 	if not type(b) is int :
 		raise TypeError ("Only integers are allowed")
-	retour = a**b
-	
-	if (retour < 0) :
-		retour = retour *(-1)
-	
-	return retour
+	return a**b
 	
