5415f99077252550419fb2011194fdf59ce4a0c6
FabHaar <fabienhaar@gmail.com>
2023-09-11 08:58:58 +0200

    Q5

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -8,7 +8,23 @@ def puissance(a,b):
 			return -1
 		else :
 			return 1
-	if a == 0 & b<0 :
+	if a == 0 & b<0 : #exception p0 puissance négative
 		raise TypeError ("0 avec une puissance négative impossible")
 	
-	return a**b
+	#return a**b
+	
+	if b < 0 :
+		bb = b* -1
+	else
+		bb = b
+	
+	retour = float(a)
+	
+	if bb != 1 :
+		for i in range(bb-1) :
+			retour = retour * a
+	
+	if b < 0 :
+		retour = 1/retour
+	
+	return retour
