3d96f0864934ea0a96bf4437c91398a6c9ba66d9
FabHaar <fabienhaar@gmail.com>
2023-09-26 09:47:57 -0400

    correction avec detection d'erreur avec les if

--- TP4/purge.sh
+++ TP4/purge.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-#Q2.1
+#2.1
 #stop tous les conteneurs
 docker stop $(docker ps -aq)
+if [ $? != 0 ]
+then
+	echo "erreur sur docker stop"
+fi
 
 #supprime tous les conteneurs
 docker rm $(docker ps -aq)
+if [ $? != 0 ]
+then
+	echo "erreur sur docker rm"
+fi
 
 #2.2
 #supprime les volumes
 docker volume prune
+if [ $? != 0 ]
+then
+	echo "erreur sur docker volume prune"
+fi
 
 #supprimme les réseaux
 docker network prune
+if [ $? != 0 ]
+then
+	echo "erreur sur docker network prune"
+fi
