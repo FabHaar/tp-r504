fbd474fa55b7d013e907cae6f1e6ecc7f67af01c
FabHaar <fabienhaar@gmail.com>
2023-09-26 09:23:33 -0400

    q2.2

--- TP4/purge.sh
+++ TP4/purge.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#Q2.1
 #stop tous les conteneurs
 docker stop $(docker ps -aq)
 
 #supprime tous les conteneurs
 docker rm $(docker ps -aq)
+
+#2.2
+#supprime les volumes
+docker volume prune
+
+#supprimme les réseaux
+docker network prune
