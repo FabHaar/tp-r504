75b5a3b7c79fdedea1e58f1ba976aa0074d0e178
FabHaar <fabienhaar@gmail.com>
2023-11-25 12:05:08 +0100

    serveur q 2.1

--- TP9/ClientTCP1.java
+++ TP9/ClientTCP1.java
@@ -5,6 +5,17 @@ public class ClientTCP
 {
 	public static void main ( String[] args )
 	{
-		
-	}
+		try
+		{
+			//q2.1
+			Socket socket = new Socket("localhost",2016);
+			DataOutpuStream dOut = new DataOutputStream(socket, getOutputStream() );
+			dOut.writeUTF("message test");
+			socket.close();
+		}
+		catch (Exception ex) 
+		{
+			System.out.println("erreur !");
+			ex.printStackTrace();
+		}
 }
