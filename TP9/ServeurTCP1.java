b380d753983f61636b70e4ac249a91a8ecc9e8b4
FabHaar <fabienhaar@gmail.com>
2023-11-25 11:57:23 +0100

    serveurtcp q 2.1

--- TP9/ServeurTCP1.java
+++ TP9/ServeurTCP1.java
@@ -5,6 +5,23 @@ public class ServeurTCP
 {
 	public static void main ( String[] args )
 	{
-		
+		try
+		{
+			//Q2.1
+			ServerSocket socketserver = new ServerSocket(2016);
+			
+			System.out.println("serveur en attente");
+			Socket socket = socketserver.accept();
+			System.out.println("Connexion d'un client");
+			
+			DataInputStream dIn = new DataInputStream(socket.getInputStream());
+			System.out.println("Message : " + dIn.readUTF());
+			socket.close();
+			socketserver.close();
+		}
+		catch (Exception ex) 
+		{
+			System.out.println("erreur !");
+			ex.printStackTrace();
 	}
 }
