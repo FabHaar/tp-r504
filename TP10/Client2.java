21aacc3773ac564f42ac77c811aecf606a60b56d
FabHaar <fabienhaar@gmail.com>
2023-12-08 05:14:24 -0500

    api rest

---
+++ TP10/Client2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+
+public class Client2
+{
+	public static void main ( String[] args )
+	{
+		try 
+		{
+			if (args.length > 0)
+			{
+				
+				CloseableHttpClient client = HttpClients.createDefault();
+				String url = "http://" + args[0];
+				HttpGet request = new HttpGet(url);
+				
+				//q4
+				System.out.println("Executing request " + request.getRequestLine());
+				CloseableHttpResponse resp = client.execute(request);
+				
+				System.out.println("Response Line : " + resp.getStatusLine());
+				System.out.println("Response Code : " + resp.getStatusLine().getStatusCode());
+				
+				//client rest
+				InputStreamReader isr = new InputStreamReader(resp.getEntity().getContent() );
+				
+			}
+			else 
+			{
+				System.out.println("Pas d'argument fourni");
+			}
+		}
+		catch (Exception ex) 
+		{
+			System.out.println("erreur !");
+			ex.printStackTrace();
+		}
+	}
+}
