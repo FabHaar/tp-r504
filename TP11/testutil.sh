a0b2648c4fe8eb2ad9126856823d99d4985961c4
FabHaar <fabienhaar@gmail.com>
2023-12-18 03:32:40 -0500

    save q1

--- TP11/testutil.sh
+++ TP11/testutil.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
-function check_utilisateur {
-	echo "Entrer le nom utilisateur à trouver : "
-	read utilisateur
-	result=$(cat /etc/passwd | grep "$utilisateur"*)
-	
-	if [ $? -eq 0]; 
-	then
-		echo "l'utilisateur existe"
-		echo "$result"
-	else
-		echo "l'utilisateur n'existe pas"
-}
-
-function get_id {
-	echo "Entrer le nom utilisateur avec id à connaitre : "
-	read utilisateur
-	result=$(id -u $utilisateur)
-	
-	if [ $? -eq 0]; 
-	then
-		echo "$result"
-	else
-		echo "l'utilisateur n'existe pas"
-}
-
+PS3='Entrer votre choix : '
 options=("1" "2" "q")
 
 select opt in "${options[@]}"
 do
 	case $opt in
 		"1")
-			check_utilisateur
+			echo "Entrer le nom utilisateur à trouver : "
+			read utilisateur
+			result=$(cat /etc/passwd | grep "$utilisateur"*)
+	
+			if [ $? -eq 0 ]; 
+			then
+				echo "l utilisateur existe"
+				echo "$result"
+			else
+				echo "l utilisateur n'existe pas"
+			fi
 			;;
 		"2")
-			get_id
+			echo "Entrer le nom utilisateur avec id à connaitre : "
+			read utilisateur
+			result=$(id -u $utilisateur)
+	
+			if [ $? -eq 0 ]; 
+			then
+				echo "$result"
+			else
+				echo "l utilisateur n'existe pas"
+			fi
 			;;
 		"q")
 			break
