0bde54c567d684087932abcb20a6a360129b7493
FabHaar <fabienhaar@gmail.com>
2024-02-08 15:21:58 +0100

    fonction validate password + connexion mysql

--- sae61-1/app_1.py
+++ sae61-1/app_1.py
@@ -1,7 +1,31 @@
+import mysql.connector
 import re
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+def validate_password(password):
+	regex1 = r"^.{6,15}$" #entre 6 et 15 caractères
+	regex2 = r".*\d.*" #au moins un chiffre
+	regex3 = r".*[a-z].*" #au moins une minuscule
+	regex4 = r".*[A-Z].*" #au moins une majuscule
+	regex5 = r".*[#%{}@].*" #au moins un de ces caractères
+	
+	tab_validation = [bool(re.match(regex1, password)), bool(re.match(regex2, password)), bool(re.match(regex3, password)), bool(re.match(regex4, password)), bool(re.match(regex5, password))]
+	return tab_validation
+	
+# Config mysql-------------------------------------------
+db_config = {
+    'host': 'serveur-mysql',
+    'user': 'root',
+    'password': 'foo',
+    'database': 'sae61',
+	'port': '3306'
+}
+# Connexion à la bdd
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+# Fin config mysql --------------------------------------
 
 # Route pour la page d'accueil
 @app.route('/')
