be8d446225e0d6d4e01b74d66dbba96f09074fc3
lucas-sofianos <lucas.sofianos@gmail.com>
2024-02-08 15:47:07 +0100

    modification def new_user / run_all / html / divers (dockerfile...)

--- sae61-1/app_1.py
+++ sae61-1/app_1.py
@@ -6,24 +6,40 @@ app = Flask(__name__)
 # Route pour la page d'accueil
 @app.route('/')
 def index():
-    return """
-    <h1>Accueil</h1>
-    <p>Bienvenue sur notre site !</p>
-    <p>Pour créer un nouveau compte, veuillez accéder à la <a href="/newuser">page de création de compte</a>.</p>
-    """
+	return """
+	<h1>Accueil</h1>
+	<p>Bienvenue sur notre site !</p>
+	<p>Pour créer un nouveau compte, veuillez accéder à la <a href="/newuser">page de création de compte</a>.</p>
+	"""
 
-@app.route('/newuser')
-def newuser():
-	#return render_template('newuser.html')
-	#return render_template('index.html')
-	
+
+# Route pour la page de création de nouveau compte
+@app.route('/newuser', methods=['GET', 'POST'])
+def new_user():
+	message = ''
+
+	if request.method == 'POST':                                       # En attente d'une requête de type POST
+	     	username = request.form['username']                        # Récupère l'identifiant saisi dans le formulaire HTML
+		email = request.form['email']                              # Récupère l'email saisi dans le formulaire HTML
+		password = request.form['password']                        # Récupère le mot de passe saisi dans le formulaire HTML
+		#LeReturn = validate_password(password)
+		#if validate_username(username) and validate_email(email) and validate_password(password):  # Valide les champs
+		#    message = 'Données valides !'
+		if validate_password(password):                            # Valide le mot de passe en utilisant la fonction validate_password
+			message = 'Mot de passe valide !'
+		else:
+			message = 'Le mot de passe doit contenir au moins 6 caractères, au moins 1 chiffre, une majuscule et une minuscule.'
+    
+	return render_template('newuser.html', message=message)            # Renvoie la page HTML 'newuser.html' avec le message à afficher
+
+
 	
 @app.route('/liste')
 def liste():
-	
+	pass
 @app.route('/connect')
 def connect():
-
+	pass
 
 
 
--- sae61-1/run_app.sh
+++ sae61-1/run_app.sh
@@ -5,4 +5,10 @@ docker run -d -it\
 		--name regex-app \
 		--network net-sae61 \
 		--mount type=bind,source="$(pwd)"/app_1.py,target=/srv/app_1.py \
-		im-regex
+		im-sae61
+
+if [ $? != 0 ]
+then
+	echo "echec de lancement du conteneur app"
+	exit 1
+fi
