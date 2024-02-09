e378a2344bb0fc99f6602e198943016ce1558cfb
FabHaar <fabienhaar@gmail.com>
2024-02-09 18:06:43 +0100

    modif variable message

--- sae61-1/python/app_1.py
+++ sae61-1/python/app_1.py
@@ -16,7 +16,7 @@ def index():
 # Route pour la page de création de nouveau compte
 @app.route('/newuser', methods=['GET', 'POST'])
 def new_user():
-	message = ''
+	message_mdp = ''
 
 	if request.method == 'POST':                                       # En attente d'une requête de type POST
 		username = request.form['username']                        # Récupère l'identifiant saisi dans le formulaire HTML
@@ -26,30 +26,30 @@ def new_user():
 		validation = fonctions.validate_password(password)                   #rempli la variable validation avec tout ce qu'il faut pour le traitement de chaque critère selon password
 		
 		check_p = True	#initilaisation d'un compteur qui servira à valider si toutes les regex sont respectées
-		message="Le mot de passe ne respecte pas les criteres suivants : "			
+		message_mdp ="Le mot de passe ne respecte pas les criteres suivants : "			
 		if not validation[0]:
 			check_p = False
-			message = message + "Entre 6 et 15 caracteres "
+			message_mdp = message_mdp + "Entre 6 et 15 caracteres "
 		if not validation[1]:
 			check_p = False
-			message = message + "Au moins un chiffre "
+			message_mdp = message_mdp + "Au moins un chiffre "
 		if not validation[2]:
 			check_p = False
-			message = message + "Au moins une minuscule"
+			message_mdp = message_mdp + "Au moins une minuscule"
 		if not validation[3]:
 			check_p = False
-			message = message + "Au moins une majuscule "
+			message_mdp = message_mdp + "Au moins une majuscule "
 		if not validation[4]:
 			check_p = False
-			message = message + "Au moins un de ces caractères : #%{}@"
+			message_mdp = message_mdp + "Au moins un de ces caractères : #%{}@"
 		
 		if check_p:
-			message = "Mot de passe valide"
+			message_mdp = "Mot de passe valide"
 		
 		
 		#if validate_username(username) and validate_email(email) and validate_password(password):  # Valide les champs
     
-	return render_template('newuser.html', message=message)            # Renvoie la page HTML 'newuser.html' avec le message à afficher
+	return render_template('newuser.html', message_mdp=message_mdp)            # Renvoie la page HTML 'newuser.html' avec le message à afficher
 
 
 	
