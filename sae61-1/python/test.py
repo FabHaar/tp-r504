34bf8cc3aa8827b4a0fd4988c1c0f960cf9129dc
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:13:47 +0100

    ajout test username

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -4,3 +4,6 @@ import fonctions as f
 def test_regex_password() :
 	assert f.validate_password("a") == [False,False,True,False,False]
 	assert f.validate_password("aA#123azs") == [True,True,True,True,True]
+
+def test_regex_username() :
+	assert f.validate_username("fabien") == [True,True,True]
