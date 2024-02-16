2eb9ee62c6f7703a9bc97a790011283214af0e33
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:17:53 +0100

    test 2 echec

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -6,4 +6,4 @@ def test_regex_password() :
 	assert f.validate_password("aA#123azs") == [True,True,True,True,True]
 
 def test_regex_username() :
-	assert f.validate_username("fabien") == [True,True,True]
+	assert f.validate_username("fabien") == [False,True,True]
