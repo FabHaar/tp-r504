91544571a5883107dac911bb58579180db4a9692
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:11:51 +0100

    test supp password

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -1,5 +1,6 @@
 import pytest
 import fonctions as f
 
-def test_1() :
+def test_regex_password() :
 	assert f.validate_password("a") == [False,False,True,False,False]
+	assert f.validate_password("aA#123azs") == [True,True,True,True,True]
