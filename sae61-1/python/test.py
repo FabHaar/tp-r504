31bc3ce7c772bfe82fba3ffe59b8909cd92bef1e
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:06:59 +0100

    j ai fermé le crochet

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -2,4 +2,4 @@ import pytest
 import fonctions as f
 
 def test_1() :
-	assert f.validate_password("a") == [False,False,True,False,False
+	assert f.validate_password("a") == [False,False,True,False,False]
