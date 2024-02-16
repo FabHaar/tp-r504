96411598fec93d4b86a982a363430403ad48eea4
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:20:05 +0100

    remis tout bon

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -2,8 +2,8 @@ import pytest
 import fonctions as f
 
 def test_regex_password() :
-	assert f.validate_password("a") == [True,False,True,False,False]
+	assert f.validate_password("a") == [False,False,True,False,False]
 	assert f.validate_password("aA#123azs") == [True,True,True,True,True]
 
 def test_regex_username() :
-	assert f.validate_username("fabien") == [False,True,True]
+	assert f.validate_username("fabien") == [True,True,True]
