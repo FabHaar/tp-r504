53f5cc850d65a7e632a9b22b94a93477196c7dbc
FabHaar <fabienhaar@gmail.com>
2024-02-16 11:16:18 +0100

    test échec

--- sae61-1/python/test.py
+++ sae61-1/python/test.py
@@ -2,7 +2,7 @@ import pytest
 import fonctions as f
 
 def test_regex_password() :
-	assert f.validate_password("a") == [False,False,True,False,False]
+	assert f.validate_password("a") == [True,False,True,False,False]
 	assert f.validate_password("aA#123azs") == [True,True,True,True,True]
 
 def test_regex_username() :
